349b9510634781de349eb8abd64b968b3dc52f43
adamd <adam.d.hinton@gmail.com>
2024-03-16 21:03:26 -0400

    method ref stuff I don't really understand

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -9,15 +9,30 @@ import java.util.function.UnaryOperator;
 public class Main {
     private static Random random = new Random();
 
+    private record Person(String first){
+        public String last(String s){
+            return first + " " + s.substring(0, s.indexOf(" "));
+        }
+    }
+
     public static void main(String[] args) {
 
 
         String[] names = {"Anna", "Bob", "Cameron", "Donald", "Eva", "Francis"};
 
+        Person tim = new Person("Tim");
+
         List<UnaryOperator<String>> list = new ArrayList<>(List.of(
 
                 String::toUpperCase,
-                s -> s += " " + getRandomChar('A', 'Z') + "."
+                s -> s += " " + getRandomChar('A', 'Z') + ".",
+                s -> s+= " " + reverse(s, 0, s.indexOf(" ")),
+                Main::reverse,
+                String::new,
+//                s-> new String("Howdy" + s),
+                String::valueOf,
+                tim::last
+
 
 
         ));
@@ -39,6 +54,14 @@ public class Main {
     private static char getRandomChar(char startChar, char endChar){
         return (char) random.nextInt((int) startChar, (int) endChar +1);
 
+    }
+
+    private static String reverse (String s){
+        return reverse(s, 0, s.length());
+    }
+
+    private static String reverse(String s, int start, int end){
+        return new StringBuilder(s.substring(start, end)).reverse().toString();
     }
 
 }
